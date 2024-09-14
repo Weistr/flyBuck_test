c8f9b033b5c4b2c9e6b0d401190be3c902709dfd
Weistr <1561267515@qq.com>
2024-09-14 17:45:18 +0800

    b

--- software/flyback_test.py
+++ software/flyback_test.py
@@ -1,3 +1,4 @@
+
 from sympy import * 
 from math import sqrt
 
@@ -5,73 +6,89 @@ from math import sqrt
 
 
 pi = 3.1415926
-#电气参数
-eff = 0.85 #电源效率
-Vo = 12 #输出电压
-Io_max = 4.2 #额定输出电流
-Pmax = Vo * Io_max / eff #最大功率
-print("Pmax = ",Pmax,"W")
 
-Vi_AC_RMS_Min = 220 * (1-0.15)
-Vi_AC_RMS_Max = 220 * (1+0.15)
+
+print("###########输入电容计算#############")
+Vo = 12 #输出电压
+Io_max = 4 #最大输出电流
+eff = 0.85 #电源效率
+Fi_AC = 50#输入交流电频率
+Vi_AC_RMS_Min = 176 #交流输入电压范围+- 20%
+Vi_AC_RMS_Max = 264
+Vbus_pp = 25 #母线电压纹波
+
+
 
 Vbus_pk_Min = Vi_AC_RMS_Min * sqrt(2)#母线电压峰值
-Vbus_pk_Max = Vi_AC_RMS_Max * sqrt(2)
-
-#输入电容计算
-Fi_AC = 50#输入交流电50Hz
-Vbus_pp = 25 #纹波
-Vbus_AVG_Min = Vbus_pk_Min - Vbus_pp/2#母线最低平均电压
-#单周期内电容充电时间
 Tc = 1/(4*Fi_AC) - asin((Vbus_pk_Min-Vbus_pp)/(Vbus_pk_Min))/(2*pi*Fi_AC)#电容充电时间
 print("Tc = ",Tc*10**3,"ms")
 Td = 1/Fi_AC/2 - Tc
 print("Td = ",Td*10**3,"ms")
 #母线电容
+Pmax = Vo * Io_max / eff #最大功率
+print("Pmax = ",Pmax,"W")
 Cbus = 2*Pmax*(1/(2*Fi_AC) - Tc)/(eff*(Vbus_pk_Min**2 - (Vbus_pk_Min-Vbus_pp)**2))
 print("Cbus = ",Cbus*10**6,"uF")
-
+print("##################################\n")
 ####################################
 #整流桥计算
 PF = 0.6 #功率因数
 Ii_RMS_Max = Pmax/(eff * Vi_AC_RMS_Min * PF)#交流输入电流
-print("Ii_RMS = ",Ii_RMS_Max,"A")
-print("Vbus_pk_Max = ",Vbus_pk_Max,"V")
+
+
 ####################################
-#根据耐压计算匝比
-#匝比&VRCD 第一次迭代计算
+print("###########耐压匝比计算############")
 mos_VDS_max = 600 #mos耐压600V
-k_vds = 0.8#降额系数
+k_vds = 0.9#降额系数
+VRCD_k = 1.3 #RCD波峰电压与反射电压比值
+VAUX = 12 #辅助绕组偏置电压
 
-mos_VDS = mos_VDS_max * k_vds#mosVDS电压
-print("mos应力=",mos_VDS,"v")
-
-VRCD_pk = mos_VDS - Vbus_pk_Max - 0.7 #RCD峰值电压
-
-VRCD_pb = 0.9*VRCD_pk#RCD波谷电压
-
-VOR = 0.7*VRCD_pb
-
+Vbus_pk_Max = Vi_AC_RMS_Max * sqrt(2)
+print("Vbus_pk_Max = ",Vbus_pk_Max,"V")
+mos_VDS = mos_VDS_max*k_vds
+print("mos应力余量=",mos_VDS_max-mos_VDS,"v")
+VRCD_pk = mos_VDS - Vbus_pk_Max#RCD波峰电压
+print("RCD波峰电压=",VRCD_pk,"V")
+VOR = VRCD_pk/VRCD_k#反射电压
+print("反射电压=",VOR,"V")
 Nps = VOR/(Vo+0.7)#匝比
-print("匝比=",Nps)
-#匝比计算完成##
-##顺带打印RCD部分参数#########################
+print("源副边匝比=",Nps)
+Nps = int(Nps)
+print("源副边匝比取整=",Nps)
+Npa = int(Nps * (Vo/VAUX))
+print("源辅边匝比=",Npa)
+print("####################################\n")
+
+#计算电流
+print("###########占空比计算校验############")
+Dmax_set = 0.5
+VF = 0.7 #输出二极管压降
+Dmax = Nps*(Vo+VF)/(Vbus_pk_Min + Nps*(Vo+VF))#依据能量守恒
+if Dmax>Dmax_set:
+    print("占空比过大，请减小匝比")
+print("最大占空比=",Dmax)
+print("################################\n")
+
+print("###########原边感量计算############")
+Krp = 0.7 #纹波电流与电流峰值的比值，CCM时小于1
+Fsw = 95*10**3 #开关频率
+Ii_pk = 2*Pmax / ((2-Krp)*Dmax*Vbus_pk_Min)
+print("峰值电流=",Ii_pk,"A")
+Ii_pp = Ii_pk * Krp #
+print("纹波电流=",Ii_pp,"A")
+
+print("################################\n")
+
+
+print("###########RCD吸收计算校验############")
+Ld = 10 * 10**-6 #漏感
+
+print("RCD波峰电压",VRCD_pk,"V")
+print("RCD波谷电压",VRCD_pb,"V")
+
 VRCD_Diod = VOR + Vbus_pk_Max
 print("RCD二极管应力=",VRCD_Diod,"V")
-print("反射电压VOR=",VOR,"V")
-print("RCD电压=",VRCD_pk,"V")
 
-
-Dmax = 0.35 #占空比
-Fsw = 85 * 10**3 #频率
-Krp = 0.9 #脉动系数，它等于一次侧电流脉动值与峰值电流的比值，CCM模式Krp<1
-Ii_AVG_Max = Pmax/(eff * Vi_AC_RMS_Min)#线圈最大平均电流
-print("电流平均值=",Ii_AVG_Max,"A")
-Ipp = Ii_AVG_Max/(1-Krp+Krp/2)**Krp#电流脉动值
-print("电流脉动值=",Ipp,"A")
-Ii_pk = Ipp/Krp
-print("电流峰值=",Ii_pk,"A")
-Ld = 10 * 10**-6 #漏感
 ELd = 1/2 * Ld * Ii_pk**2#漏感能量
 VRCD_RMS = (VRCD_pk + VRCD_pb)/2 #估算，不准
 RCD_R  = VRCD_RMS**2 / (ELd*Fsw)
@@ -118,7 +135,7 @@ def RCD_rechose():
 if(chr == 'y'):
     RCD_rechose()
 
-
+print("################################\n")
 
 
 
@@ -149,7 +166,7 @@ print("磁芯截面积估算为",Sj,"cm2","=",Sj*10**2,"mm2")
 #AP法选磁芯，Ap = Aw*Ae
 Kw = 0.35 #窗口利用率
 J = 400 #电流密度A/cm2
-Bmax = 0.2 #磁饱和
+Bmax = 0.3 #磁饱和
 AP = 0.433*(1+eff)*Pmax/(eff*Kw*Dmax*J*Bmax*Krp*Fsw)*10**4
 print("磁芯AP为",AP,"cm4")
 
@@ -165,6 +182,10 @@ class mesuredMagCore :
     L1 = 1570 * 10**-6 #在20匝时的电感量
     #N2 = 63
     #L2 = 778 * 10**-6 #磨气隙后在63匝时的电感量 
+#磁饱和校验
+def oth_calc_Bmax():
+    Bmax = Lp*Ii_pk/(Np*Ae)
+    print("最大磁感应强度Bmax=",Bmax,"T")
 
 #计算气隙
 def oth_calc_lg():
@@ -176,8 +197,8 @@ def oth_calc_lg():
 #计算初级线圈匝数
 Np = Lp*Ii_pk / (Bmax*Ae)
 print("初级线圈匝数=",Np)
-Ns = Np / Nps
-print("次级线圈匝数=",Ns)
+#Ns = Np / Nps_
+#print("次级线圈匝数=",Ns)
 
 #重定匝数
 def npsReChose():
@@ -187,10 +208,7 @@ def npsReChose():
 
 
 
-#磁饱和校验
-def oth_calc_Bmax():
-    Bmax = Lp*Ii_pk/(Np*Ae)
-    print("最大磁感应强度Bmax=",Bmax,"T")
+
 #####END###############################
 
 #气隙计算
